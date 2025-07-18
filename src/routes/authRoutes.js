540cca5c1c1570373197d8428726726b6d65ef89
SaubhagyaSapkota <saubhagyasapkota444@gmail.com>
2025-07-18 14:03:33 +0545

    small correction

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -16,12 +16,13 @@ import createUploadMiddleware from "../middleware/multer.middleware.js";
 import {uploadFile, uploadImages} from "../controller/cloudinary.js"
 const route = express.Router();
 
-// routes for each methods
 
+// routes for each methods
 route.get("/", getAllUser);
 route.post("/register", zodValidateRequest(authSchemaZod), userRegister);
 route.post("/login", zodValidateRequest(loginSchemaZod), userlogin);
 
+// route for image upload
 const feedbackUpload = createUploadMiddleware({
   allowedTypes: ["image/jpeg", "image/png", "image/jpg", "image/webp"],
   maxFileSize: 10 * 1024 * 1024,
@@ -30,6 +31,7 @@ const feedbackUpload = createUploadMiddleware({
 })
 route.post("/upload-images", feedbackUpload.array("images"), uploadImages)
 
+// route for pdf upload
 const fileUpload = createUploadMiddleware({
   allowedTypes: ["application/pdf"],
   maxFileSize: 20 * 1024 * 1024,
@@ -38,6 +40,7 @@ const fileUpload = createUploadMiddleware({
 });
 route.post("/upload-documents", fileUpload.array("documents"), uploadFile)
 
+// routes
 route.post("/profile", authenticate, userProfile);
 route.put("/profile", authenticate, userUpdate);
 route.post("/logout", authenticate, userLogout);
